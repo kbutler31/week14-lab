c2492cc73f6c5f22411fa2807e0233430281e458
Kaleb A Butler <134346575+kbutler31@users.noreply.github.com>
2024-06-24 19:54:58 -0500

    adding lab info to github

---
+++ week14-lab/FEFunctionBasedLabpt2/src/Nav.js
@@ -0,0 +1,14 @@
+
+
+export default function Nav(){
+  
+    return (
+      <nav className="navBar">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    )
+}
